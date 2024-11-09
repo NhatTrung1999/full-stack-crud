57c9ef1e5c3b06542189269751500b4ef57b5063
Nhat Trung <trungnguyen15051999@gmail.com>
2024-11-09 20:45:48 +0700

    'abc'

---
+++ src/components/post-delete.tsx
@@ -0,0 +1,18 @@
+import { deletePost } from "@/app/actions/posts";
+
+interface PostDeleteProps {
+  id: string;
+}
+
+export default function PostDelete({ id }: PostDeleteProps) {
+  const deleteAction = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    deletePost(id);
+  };
+
+  return (
+    <form onSubmit={deleteAction}>
+      <button type="submit">Delete</button>
+    </form>
+  );
+}
